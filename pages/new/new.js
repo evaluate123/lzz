9ef1c42eefa733b2727c2d9f93de344efa09a45c
unknown <1659654037@qq.com>
2019-12-22 14:56:55 +0800

    lzz

---
+++ pages/new/new.js
@@ -0,0 +1,20 @@
+Page({
+  data: {
+
+  },
+  
+  tiaozhuan: function () {
+    
+    wx.navigateTo({
+      url: '../jianshen/new3/new3',
+    })
+   
+  },
+
+  tiaozhuan2:function(){
+
+    wx.navigateTo({
+      url: '../jianshen/new4/new4',
+    })
+  }
+})
